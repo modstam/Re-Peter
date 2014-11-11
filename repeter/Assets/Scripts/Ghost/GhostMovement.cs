0c6d1b2ae550488c5a18e8db61f28ba1a4285d39
modstam <modstam@live.com>
2014-11-11 20:16:12 +0100

    First commit!
    Added the project from local files

---
+++ repeter/Assets/Scripts/Ghost/GhostMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMovement : MonoBehaviour {
+
+
+	/** Character movement traits */
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	
+
+	/**
+	 * takes a Vector3 holding a direction and moves towards it
+	 */
+	public void MoveTowards(Vector3 direction, bool jump){
+
+		Vector3 mDirection = direction;
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			mDirection *= speed;
+			if (jump)
+				mDirection.y = jumpSpeed;		
+		}
+		mDirection.y -= gravity * Time.deltaTime;
+
+		controller.Move(mDirection*10*Time.deltaTime);
+		
+		
+	}
+}
