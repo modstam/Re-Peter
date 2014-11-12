99dbfad4ec996d22a436de69fc99b47b5e989ea2
modstam <modstam@live.com>
2014-11-12 17:44:05 +0100

    Introduced Interpolation-Ghosting
    Switched from movement-based ghosting to interpolation based. See
    GhostMainController.Update()

--- repeter/Assets/Scripts/Ghost/GhostMainController.cs
+++ repeter/Assets/Scripts/Ghost/GhostMainController.cs
@@ -8,7 +8,7 @@ public class GhostMainController : MonoBehaviour {
 	public int stop;
 	public int nextElem;
 	public float currentTime = 0.0f;
-	public float locationPrecision = 0.3f;
+	public float locationPrecision = 0.0f;
 	public State nextState;
 	public float nextTimeGoal = 0.0f;
 	public bool init = false;
@@ -37,24 +37,32 @@ public class GhostMainController : MonoBehaviour {
 		diff.z = Mathf.Abs (diff.z);
 
 		if(!(diff.x < locationPrecision && diff.y < locationPrecision && diff.z < locationPrecision)){
-			if(currentTime >= nextState.stateTime){
-				GhostMovement movementComponent = GetComponent<GhostMovement>();
+			//if(currentTime >= nextState.stateTime){
+				//GhostMovement movementComponent = GetComponent<GhostMovement>();
 				
-					if(movementComponent){ //sanity check
-					Vector3 direction = nextState.getPosition() - this.transform.position;
+			//		if(movementComponent){ //sanity check
+			//		Vector3 direction = nextState.getPosition() - this.transform.position;
 					//direction = transform.TransformDirection(direction);
-					movementComponent.MoveTowards(direction, nextState.jump);
-				}else Debug.Log ("Ghost doesnt have a movement component");
+					//movementComponent.MoveTowards(direction, nextState.jump);
+					//this.transform.position = new Vector3(Mathf.Lerp (nextState.getPosition().x, this.transform.position.x, 0.5f),
+					//	                                  Mathf.Lerp (nextState.getPosition().y, this.transform.position.y, 0.5f),
+					//	                                      Mathf.Lerp (nextState.getPosition().z, this.transform.position.z, 0.5f));
+			//	}else Debug.Log ("Ghost doesnt have a movement component");
 				
-			}//else Debug.Log("Something went horribly wrong");
+			//}//else Debug.Log("Something went horribly wrong");
 
+
+				this.transform.position = new Vector3(Mathf.Lerp (nextState.getPosition().x, this.transform.position.x, Time.deltaTime ),
+				                                      Mathf.Lerp (nextState.getPosition().y, this.transform.position.y, Time.deltaTime),
+				                                      Mathf.Lerp (nextState.getPosition().z, this.transform.position.z, Time.deltaTime));
+				//Time.deltaTime
 		}
 		else{
 
 			if(nextElem != stop){
 				if(currentTime >= nextState.stateTime){
-					//this.transform.position = nextState.getPosition();
-					//this.transform.rotation = nextState.getRotation();
+					this.transform.position = nextState.getPosition();
+					this.transform.rotation = nextState.getRotation();
 
 					this.nextState = states[nextElem];
 					this.nextTimeGoal = nextState.stateTime;
