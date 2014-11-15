e8a5549ecef6e1f768c45cf79c45423244948b60
Robin Chowdhury <robinch88@gmail.com>
2014-11-15 16:25:13 +0100

    Camera check and first person on third person

---
+++ repeter/Assets/Scripts/Character/ChooseCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class VRCamera : MonoBehaviour
+{
+	public Transform riftCameraPrefab;
+	
+	void Awake()
+	{
+		// Instantiate OVRCameraRig to detect Oculus Rift
+		Transform riftCamera = (Transform) Instantiate(riftCameraPrefab, transform.position, transform.rotation);
+		riftCamera.parent = transform;
+		
+		if (OVRManager.display.isPresent) {
+			// Disable normal camera if Rift is connected
+			transform.Find("Main Camera").gameObject.SetActive(false);
+		} else {
+			// Destroy OVRCamraRift if Rift is not connected
+			DestroyImmediate(riftCamera.gameObject);
+		}
+	}
+}
