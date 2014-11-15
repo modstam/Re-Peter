48893a680af9b115f5cbb26472d9b75e5184194b
Mattias <Mattias.Folke@gmail.com>
2014-11-15 14:52:58 +0100

    tutorial improved

---
+++ repeter/Assets/doorbutton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorbutton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+		//Debug.Log("Collided with button");
+		foreach (Transform child in transform)
+			child.gameObject.SetActive(false);
+	}
+
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log("leaving button");
+		gameObject.SetActiveRecursively(true);
+		gameObject.SetActive(true);
+	}
+}
