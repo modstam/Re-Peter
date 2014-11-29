b9180826fce693aacab12dd2847390aee7f0f575
modstam <modstam@live.com>
2014-11-29 19:23:20 +0100

    Improved the platforms, added a new StickyPlatform-script

--- repeter/Assets/Scripts/Platform/PlatformMover.cs
+++ repeter/Assets/Scripts/Platform/PlatformMover.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlatformMover : MonoBehaviour {
 
@@ -8,6 +9,8 @@ public class PlatformMover : MonoBehaviour {
 	public Transform destination;
 	public float speed = 0.1f; // movements per second
 	bool moveToDest = false;
+
+	public List<Rigidbody> objectsOnPlatform = new List<Rigidbody>();
 	
 	void OnTriggerEnter(Collider other) {
 		moveToDest = true;
@@ -20,14 +23,21 @@ public class PlatformMover : MonoBehaviour {
 	void FixedUpdate(){
 
 		if(moveToDest){
+
 			if(!(platform.transform.position == destination.transform.position)){
-				platform.transform.position = Vector3.MoveTowards(platform.transform.position, destination.position, speed);
+				StickyPlatform stickyPlatform = platform.gameObject.GetComponent<StickyPlatform>();
+				if(stickyPlatform){
+					stickyPlatform.moveTowards(destination.position, speed);
+				}
 			}
 		} else {
-				if(!(platform.transform.position == origin.transform.position)){
-					platform.transform.position = Vector3.MoveTowards(platform.transform.position, origin.position, speed);
+			if(!(platform.transform.position == origin.transform.position)){
+				StickyPlatform stickyPlatform = platform.gameObject.GetComponent<StickyPlatform>();
+				if(stickyPlatform){
+					stickyPlatform.moveTowards(origin.position, speed);
+				}
 			}
 		}
-	}
+	}	
 }
 
---
+++ repeter/Assets/Scripts/Platform/StickyPlatform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class StickyPlatform : MonoBehaviour {
+
+	public List<Rigidbody> objectsOnPlatform = new List<Rigidbody>();
+	public bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void moveTowards(Vector3 destination, float speed){
+
+		foreach(Rigidbody body in objectsOnPlatform){
+			body.transform.position = Vector3.MoveTowards(body.transform.position, destination, speed);
+		}
+		transform.position = Vector3.MoveTowards(transform.position, destination, speed);
+	}
+
+
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log("Found contact on platform");
+		if(collision.gameObject.rigidbody){ //Does this object have a rigid body?
+			objectsOnPlatform.Add(collision.gameObject.rigidbody); //add it to list
+		}
+	}
+	
+	void OnCollisionExit(Collision collision){
+		if(collision.gameObject.rigidbody){ //Does this object have a rigid body?
+			objectsOnPlatform.Remove(collision.gameObject.rigidbody); //remove it from list
+		}
+	}
+
+}
