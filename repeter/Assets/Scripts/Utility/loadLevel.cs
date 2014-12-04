eda1e5e928cc9124aca3fdbee205174b46dbc4a0
Mattias <Mattias.Folke@gmail.com>
2014-12-04 16:39:19 +0100

    super boost

---
+++ repeter/Assets/Scripts/Utility/loadLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevel : MonoBehaviour {
+
+
+	public float fadeSpeed = 1.5f;
+	public int levelnum;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("completed level");
+		Application.LoadLevel(levelnum); //TODO change this
+	}
+
+}
