56e2063c0cc800f5f5c3e3c61fdaff1a0303d6ee
Robin Chowdhury <robinch88@gmail.com>
2014-12-12 19:27:21 +0100

    disappearing platforms work

---
+++ repeter/Assets/Scripts/Platform/PlatformToggler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformToggler : MonoBehaviour {
+
+	public Transform platform;
+	public bool showWhenOnTrigger;
+	private bool trig;
+	
+	
+	void OnTriggerEnter(Collider other) {
+		trig = true;
+	}
+	
+	void OnTriggerExit(Collider other) {
+		trig = false;
+	}
+	
+	void FixedUpdate(){
+		platform.gameObject.SetActive(!(trig ^ showWhenOnTrigger));
+	}
+}
