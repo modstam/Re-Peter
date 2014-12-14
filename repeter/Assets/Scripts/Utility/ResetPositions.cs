896c37a4511f87412d09c3f05d0bbbe9aa0cd54f
Robin Chowdhury <robinch88@gmail.com>
2014-12-15 00:17:13 +0100

    ResetPosition

--- repeter/Assets/Scripts/Utility/ResetPositions.cs
+++ repeter/Assets/Scripts/Utility/ResetPositions.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ResetPositions : MonoBehaviour {
 
+/*
+ * Drag and drop platforms that should reset on "death"
+ * in to the transforms array
+ */
+public class ResetPositions : MonoBehaviour {	
+
+	Vector3[] posList;
+	public Transform[] transforms;
 
 	// Use this for initialization
 	void Start () {
-		foreach (Transform child in transform) {
-			//child.position;
+		posList = new Vector3[transforms.Length];
+		for(int i = 0; i < transforms.Length; i++) {
+			posList[i] = transforms[i].position;
 		}
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void ResetAllPositions() {
-	}
+	public void Reset(){
+		for(int i = 0; i < transforms.Length; i++) {
+			transforms[i].position = posList[i];
+		}
+	}	
 }
